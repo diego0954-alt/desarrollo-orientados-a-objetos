82eede8e0bd50d0f2925790c16355f28f24c01da
diego0954-alt <diego0954@icloud.com>
2026-08-14 10:24:35 -0400

    suma

---
+++ mis trabajos/calculadora.java
@@ -0,0 +1,11 @@
+public class calculadora {
+ 
+    public static void MostarSuma(int a, int b) {
+        System.out.println("la suma es:" + (a + b));
+    }
+    public static void main(String[] args) {
+        MostarSuma(5, 10);
+        MostarSuma(100, 250);
+    }
+
+}
