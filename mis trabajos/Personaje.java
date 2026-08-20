5522ec692c819bc2e97af9e8782663ded91ded8c
diego0954-alt <diego0954@icloud.com>
2026-08-20 17:15:19 -0400

    Add files via upload

--- mis trabajos/Personaje.java
+++ mis trabajos/Personaje.java
@@ -3,15 +3,20 @@ public class Personaje {
     String apellido; 
     int vida;
     boolean esta_vivo = true;
+    private int edad;
 
 
     public void setearVida(int vida){
         this.vida = vida;
         if(vida < 0){
-            this.vida = 0;
+            System.out.println("la edad no puede ser negativa, asi que le asignamos a tu perosonaje 18");
+            this.vida = 18;
         } else {
             this.vida = vida;
         }
 
     }
 }
+
+    public int obtenerEdad;
+        return
