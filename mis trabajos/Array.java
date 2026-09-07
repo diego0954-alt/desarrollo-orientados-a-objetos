8d74e3cf05095b362c349231dfcb7df861c863cb
diego0954-alt <diego0954@icloud.com>
2026-09-07 10:02:33 -0300

    Array

---
+++ mis trabajos/Array.java
@@ -0,0 +1,18 @@
+public class Array {
+    public static void main (String[] args){
+        int[] notas = new int[5];
+
+        notas[0] = 65;
+        notas[1] = 70;
+        notas[2] = 90;
+        notas[3] = 10;
+        notas[4] = 40;
+
+        System.out.println("la primera nota es: " + notas[3]);
+
+        for (int i = 0; i < notas.length; i++) {
+            
+        }
+
+    }
+}
